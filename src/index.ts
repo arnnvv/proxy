8ff13f0364fc6951657cedb3f7b7a8e6bcbe7bcd
arnnvv <arnav.900@protonmail.com>
2024-06-28 14:09:47 +0530

    headers

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,13 @@ app.use(
   ): Promise<void> => {
     if (!c.env.TARGET_URL || c.env.TARGET_URL.length === 0)
       throw new Error("TARGET_URL is not set");
+
+    c.header("Access-Control-Allow-Origin", "*");
+    c.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    c.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    c.header("Access-Control-Allow-Credentials", "true");
+    c.header("Access-Control-Expose-Headers", "Content-Length, Content-Range");
+
     await next();
   },
 );
