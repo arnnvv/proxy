cd1c0c6655ac1cf0d856c365f1d1b95624a9054b
arnnvv <arnav.900@protonmail.com>
2024-06-28 13:50:21 +0530

    envSetUp

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,37 @@
-import { Hono } from 'hono'
+import { Context, Hono, Next, TypedResponse } from "hono";
+import { StatusCode } from "hono/utils/http-status";
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    TARGET_URL: string;
+  };
+}>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(
+  async (
+    c: Context<
+      {
+        Bindings: {
+          TARGET_URL: string;
+        };
+      },
+      never,
+      {}
+    >,
+    next: Next,
+  ): Promise<void> => {
+    if (!c.env.TARGET_URL || c.env.TARGET_URL.length === 0)
+      throw new Error("TARGET_URL is not set");
+    await next();
+  },
+);
 
-export default app
+app.get(
+  "/",
+  (c: Context): Response & TypedResponse<any, StatusCode, "text"> => {
+    console.log(c.env.TARGET_URL);
+    return c.text(c.env.TARGET_URL);
+  },
+);
+
+export default app;
